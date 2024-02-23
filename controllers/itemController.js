0d32c83db4ec9090c5595e27e6d01d589eba9ac8
basnetrajpradip <basnetrajpradip@gmail.com>
2024-02-23 09:53:24 +0545

    create controller for item

---
+++ controllers/itemController.js
@@ -0,0 +1,48 @@
+const Item = require("../models/item");
+const Category = require("../models/category");
+
+const asyncHandler = require("express-async-handler");
+
+exports.index = (req, res, next) => {
+  res.render("index");
+};
+
+//Display list of all items
+exports.item_list = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item list");
+};
+
+//Display detail for a specific item.
+exports.item_detail = (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Item detail: ${req.params.id}`);
+};
+
+//Display item create form on GET.
+exports.item_create_get = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item create GET");
+};
+
+//Handle item create on POST.
+exports.item_create_post = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item create POST");
+};
+
+//Display item delete form on GET.
+exports.item_delete_get = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item delete GET");
+};
+
+//Handle item delete on POST.
+exports.item_create_post = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item delete POST");
+};
+
+//Display item update form on GET.
+exports.item_create_get = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item update GET");
+};
+
+//Handle item create on POST.
+exports.item_create_post = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Item update POST");
+};
