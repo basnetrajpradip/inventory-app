44134ac230dfb38740da60480e20255646ac107c
basnetrajpradip <basnetrajpradip@gmail.com>
2024-02-23 11:15:22 +0545

    Update route configurations and database connection

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const inventoryRouter = require("./routes/inventory");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
+
+//Set up  mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +36,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
