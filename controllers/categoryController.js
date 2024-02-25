bf1c06bebc6635b2de3e434f182a09619581a1bb
basnetrajpradip <basnetrajpradip@gmail.com>
2024-02-25 17:16:00 +0545

    Update item and category controller functions

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,26 +1,60 @@
 const Category = require("../models/category");
-
+const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 //Display list of all Categories
-exports.category_list = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category list");
-};
+exports.category_list = asyncHandler(async (req, res, next) => {
+  const allCategories = await Category.find({}, "name").sort({ name: 1 }).exec();
+  res.render("categories_list", {
+    title: "All Categories",
+    categories: allCategories,
+  });
+});
 
 //Display detail for a specific category.
-exports.category_detail = (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: category detail: ${req.params.id}`);
-};
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const [category, categoryItems] = await Promise.all([Category.findById(req.params.id).exec(), Item.find({ category: req.params.id }).sort({ name: 1 }).exec()]);
+  res.render("category_detail", {
+    category: category,
+    categoryItems: categoryItems,
+  });
+});
 
 //Display category create form on GET.
 exports.category_create_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category create GET");
+  res.render("category_form", { title: "Create Category" });
 };
 
 //Handle category create on POST.
-exports.category_create_post = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category create POST");
-};
+exports.category_create_post = [
+  //Validate and sanitize the field.
+  body("name", "Category must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty").trim().isLength({ min: 1 }).escape(),
+
+  //Process after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    //Extract errors from the request.
+    const errors = validationResult(req);
+
+    //Create new Category object with escaped and trimmed data.
+    const category = new Category({ name: req.body.name, description: req.body.description });
+
+    if (!errors.isEmpty()) {
+      //There are errors. Render the form again with the sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        errors: errors.array(),
+        category: category,
+      });
+      return;
+    } else {
+      //Data is valid.
+      await category.save();
+      res.redirect(category.url);
+    }
+  }),
+];
 
 //Display category delete form on GET.
 exports.category_delete_get = (req, res, next) => {
@@ -33,11 +67,44 @@ exports.category_delete_post = (req, res, next) => {
 };
 
 //Display category update form on GET.
-exports.category_update_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category update GET");
-};
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  const category = await Category.findById(req.params.id).exec();
+  if (category === null) {
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("category_form", {
+    title: "Update Category",
+    category: category,
+  });
+});
 
-//Handle category create on POST.
-exports.category_update_post = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category update POST");
-};
+//Handle category update on POST.
+exports.category_update_post = [
+  //Validate and sanitize the field.
+  body("name", "Category must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty").trim().isLength({ min: 1 }).escape(),
+  //Process after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    //Extract errors from the request.
+    const errors = validationResult(req);
+
+    //Create new Category object with escaped and trimmed data.
+    const category = new Category({ name: req.body.name, description: req.body.description, _id: req.params.id });
+
+    if (!errors.isEmpty()) {
+      //There are errors. Render the form again with the sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        errors: errors.array(),
+        category: category,
+      });
+      return;
+    } else {
+      //Data is valid.
+      await Category.findByIdAndUpdate(req.params.id, category, {});
+      res.redirect(category.url);
+    }
+  }),
+];
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,31 +1,91 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
-
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
-exports.index = (req, res, next) => {
-  res.render("index");
-};
+exports.index = asyncHandler(async (req, res, next) => {
+  const [numCategories, numItems] = await Promise.all([Category.countDocuments({}).exec(), Item.countDocuments({}).exec()]);
+
+  res.render("index", {
+    category_count: numCategories,
+    item_count: numItems,
+  });
+});
 
 //Display list of all items
-exports.item_list = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item list");
-};
+exports.item_list = asyncHandler(async (req, res, next) => {
+  const allItems = await Item.find({}, "name category").populate("category").sort({ name: 1 }).exec();
+  res.render("item_list", {
+    title: "All Items",
+    items: allItems,
+  });
+});
 
 //Display detail for a specific item.
-exports.item_detail = (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Item detail: ${req.params.id}`);
-};
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate("category").exec();
+  res.render("item_detail", {
+    item: item,
+  });
+});
 
 //Display item create form on GET.
-exports.item_create_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create GET");
-};
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+  const categories = await Category.find({}, "name").sort({ name: 1 }).exec();
+  res.render("item_form", {
+    title: "Create Item",
+    categories: categories,
+  });
+});
 
 //Handle item create on POST.
-exports.item_create_post = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create POST");
-};
+exports.item_create_post = [
+  //validate and sanitize the field.
+  body("name", "Category must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("category", "Category must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("price")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Price must not be empty.")
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a numeric value")
+    .custom((value, { req }) => {
+      if (value <= 0) {
+        throw new Error("Price must be greater than 0.");
+      }
+      return true;
+    })
+    .escape(),
+  body("stock").trim().isLength({ min: 1 }).withMessage("In Stock value must not be empty").isNumeric({ min: 0 }).withMessage("In Stock must be a numeric value").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const categories = await Category.find({}, "name").sort({ name: 1 }).exec();
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("item_form", {
+        title: "Create Item",
+        categories: categories,
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 //Display item delete form on GET.
 exports.item_delete_get = (req, res, next) => {
@@ -38,11 +98,66 @@ exports.item_delete_post = (req, res, next) => {
 };
 
 //Display item update form on GET.
-exports.item_update_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update GET");
-};
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  const [item, categories] = await Promise.all([Item.findById(req.params.id).exec(), Category.find({}, "name").sort({ name: 1 }).exec()]);
+  if (item === null) {
+    const err = new Error("Item not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("item_form", {
+    title: "Update Item",
+    item: item,
+    categories: categories,
+  });
+});
 
 //Handle item create on POST.
-exports.item_update_post = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update POST");
-};
+exports.item_update_post = [
+  //validate and sanitize the field.
+  body("name", "Category must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("category", "Category must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("price")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Price must not be empty.")
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a numeric value")
+    .custom((value, { req }) => {
+      if (value <= 0) {
+        throw new Error("Price must be greater than 0.");
+      }
+      return true;
+    })
+    .escape(),
+  body("stock").trim().isLength({ min: 1 }).withMessage("In Stock value must not be empty").isNumeric({ min: 0 }).withMessage("In Stock must be a numeric value").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const categories = await Category.find({}, "name").sort({ name: 1 }).exec();
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("item_form", {
+        title: "Create Item",
+        categories: categories,
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await Item.findByIdAndUpdate(req.params.id, item, {});
+      res.redirect(item.url);
+    }
+  }),
+];
