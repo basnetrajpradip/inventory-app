0887f73152b40dc0c6850a7ca1ab02faa82ce54b
basnetrajpradip <basnetrajpradip@gmail.com>
2024-02-26 17:14:44 +0545

    Update app configuration

--- app.js
+++ app.js
@@ -8,10 +8,32 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const inventoryRouter = require("./routes/inventory");
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 //Set up  mongoose connection
 const mongoose = require("mongoose");
